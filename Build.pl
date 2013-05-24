fa4d39ed5fcb2f4b5eb9f52189bb6e8fbac49ab4
Scot Dalton <scotdalton@gmail.com>
2013-05-24 15:26:34 -0400

    Add abstract

--- Build.pl
+++ Build.pl
@@ -6,6 +6,7 @@ my $builder = Module::Build->new(
     module_name         => 'NYU::Libraries::PDS',
     license             => 'perl',
     dist_author         => 'Scot Dalton',
+    dist_abstract       => 'NYU custom PDS controller.',
     build_requires => {
       'Test::More' => '0.98',
       'Devel::Cover' => '1.03',
