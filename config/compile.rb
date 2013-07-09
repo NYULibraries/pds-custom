cc142e225e793c1f6541126c02a938dbc6830fc5
Scot Dalton <scotdalton@gmail.com>
2013-07-09 15:32:02 -0400

    Update compile script.

--- config/compile.rb
+++ config/compile.rb
@@ -1,21 +1,25 @@
 require 'compass'
 # Need to require bootstrap before sprockets loads
 require 'bootstrap-sass'
+require 'nyulibraries_assets'
 require 'fileutils'
 require 'microservice_precompiler'
-precompiler = MicroservicePrecompiler::Builder.new
-precompiler.compass_build
 # Copy images and css to dist
 %w(css images).each do |asset|
   FileUtils.rm_rf "./dist/#{asset}"
   FileUtils.cp_r "./assets/#{asset}", "./dist"
 end
-precompiler.build_path = "./dist"
 nyulibraries_assets_javascripts_path = 
   "#{Compass::Frameworks['nyulibraries_assets'].stylesheets_directory}/../javascripts"
-bootstrap_javascripts_path = 
-  "#{Compass::Frameworks['bootstrap'].stylesheets_directory}/../javascripts"
+bootstrap_assets_base = "#{Compass::Frameworks['bootstrap'].stylesheets_directory}/.."
+bootstrap_javascripts_path = "#{bootstrap_assets_base}/javascripts"
+bootstrap_images_path = "#{bootstrap_assets_base}/images"
+# Copy the bootstrap glyphicons to dist
+FileUtils.cp_r "#{bootstrap_images_path}", "./dist"
 pds_javascripts_path = "./assets/javascripts"
+precompiler = MicroservicePrecompiler::Builder.new
+precompiler.compass_build
+precompiler.build_path = "./dist"
 precompiler.send(:sprockets_env).append_path bootstrap_javascripts_path
 precompiler.send(:sprockets_env).append_path nyulibraries_assets_javascripts_path
 precompiler.send(:sprockets_env).append_path pds_javascripts_path
