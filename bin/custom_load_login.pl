51c3b8ff9470b8317d2b00c25242120de568f9c6
Scot Dalton <scotdalton@gmail.com>
2013-07-15 14:45:50 -0400

    Update glue scripts signature to take the URL :scream:

--- bin/custom_load_login.pl
+++ bin/custom_load_login.pl
@@ -16,12 +16,10 @@ use PDSUtil qw(getEnvironmentParams);
 use PDSParamUtil;
 
 sub custom_load_login {
-  my ($session_id, $institute, $calling_system, $params) = @_;
+  my ($session_id, $institute, $calling_system, $params, $target_url) = @_;
   my $pds_directory = getEnvironmentParams('pds_directory');
   my $conf = parse_conf("$pds_directory/config/pds/nyu.conf");
   $calling_system ||= PDSParamUtil::getAndFilterParam('calling_system');
-  my $target_url = PDSParamUtil::queryUrl();
-  $target_url = '' if $target_url eq '?';
   my $session_controller = NYU::Libraries::PDS::controller($conf, $institute, 
     $calling_system, $target_url, $session_id);
   $session_controller->login();
