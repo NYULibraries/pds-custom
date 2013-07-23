3023e3010e13f6cfcbe849cdeb7372466c51ec8f
Scot Dalton <scotdalton@gmail.com>
2013-07-23 10:25:32 -0400

    Add comments :smile:

--- lib/NYU/Libraries/Util.pm
+++ lib/NYU/Libraries/Util.pm
@@ -13,12 +13,19 @@ our @EXPORT_OK = qw(trim xml_encode parse_conf save_permanent_eshelf_records whi
 # global lookup hash
 my %ESCAPES = ('&' => '&amp;', '"' => '&quot;');
 
+# Function to encode quotes and ampersands.
+# Usage:
+#   my $xml = xml_encode($unencoded_string);
 sub xml_encode {
   my ($string) = @_;
   $string =~ s/([&"])/$ESCAPES{$1}/ge;
   return $string;
 }
 
+# Function to whitelist institutions.
+# Returns institution if whitelisted, undef if not.
+# Usage:
+#   my $whitelisted_institution = whitelist_institution($wild_institution);
 sub whitelist_institution {
   my ($institute) = @_;
   return undef unless $institute;
@@ -26,6 +33,9 @@ sub whitelist_institution {
   ( grep { $_ eq $institute} @institutes ) ? $institute : undef;
 }
 
+# Function to parse the PDS configuration file
+# Usage:
+#   my $conf = parse_conf($conf_file);
 sub parse_conf {
   my ($file_name, $defaults) = @_;
   return undef if (!(open(OPEN_FILE, $file_name)));
@@ -57,6 +67,9 @@ sub parse_conf {
   return $conf;
 }
 
+# Function to trim a given string
+# Usage:
+#   my $trimmed_string = trim($extra_spaces_string);
 sub trim {
   my ($string) = @_;
   $string =~ s/^\s+//;
@@ -64,29 +77,4 @@ sub trim {
   return $string;
 }
 
-sub save_permanent_eshelf_records {
-  my ($conf, $pds_handle, $tsetse_handle, $tsetse_credentials) = @_;
-  return 0 if !defined $conf;
-  if ($tsetse_handle && $pds_handle) {
-    my $tsetse_url = $conf->{'tsetse_url'};
-    return 0 unless defined $tsetse_url;
-    my $tsetse_save_permanent_url = "$tsetse_url/records/save_permanent.json";
-    # Pack cookies (semi colon delimited list of name value pairs)
-    # Pack tsetse_handle, PDS_HANDLE, and tsetse_credentials (if logging in via tsetse[saves a call to PDS]).
-    my $cookies = "tsetse_handle=$tsetse_handle; domain=.library.nyu.edu; path=/, ".
-        "PDS_HANDLE=$pds_handle; domain=.library.nyu.edu; path=/, ";
-    $cookies .= "tsetse_credentials=$tsetse_credentials; domain=.library.nyu.edu; path=/, " if $tsetse_credentials;
-    # PUT the request to the eshelf and get the response.
-    my $user_agent = LWP::UserAgent->new();
-    my $url = URI::URL->new($tsetse_save_permanent_url);
-      my $headers = HTTP::Headers->new(
-      'Content-Type' => 'application/json',
-      'Cache-Control' => 'no-cache',
-      'Cookie' => $cookies);
-    my $request = HTTP::Request->new("PUT", $url, $headers);
-    my $response = $user_agent->request($request);
-    return 0 unless ($response->is_success);
-    return 1;
-  }
-}
 1;
