1810ac8876483f0d8524c0b841eefd64223971ba
Scot Dalton <scotdalton@gmail.com>
2013-08-27 12:35:13 -0400

    Point to master :hibiscus:

--- jenkins.sh
+++ jenkins.sh
@@ -8,7 +8,7 @@ fi
 # Clone it (again)
 cd vendor && git clone git@github.com:NYULibraries/pds.git pds-core;
 # Use the dev branch
-cd pds-core && git checkout sp-3.1.4-120726-development;
+cd pds-core && git checkout master;
 # Get out of there
 cd ../../;
 
