a115ab43eb99ae4a85a03b9f59543d46a60c82a4
Scot Dalton <scotdalton@gmail.com>
2013-08-30 16:31:56 -0400

    Remove old style tsetse eshelf methods :metal:

--- lib/NYU/Libraries/Util.pm
+++ lib/NYU/Libraries/Util.pm
@@ -77,28 +77,4 @@ sub trim {
   return $string;
 }
 
-sub save_permanent_eshelf_records {
-  my ($self, $conf, $pds_handle, $tsetse_handle, $tsetse_credentials) = @_;
-  return 0 if !defined $conf;
-  if ($tsetse_handle && $pds_handle) {
-    my $tsetse_url = $conf->{'tsetse_url'};
-    return 0 unless defined $tsetse_url;
-    my $tsetse_save_permanent_url = "$tsetse_url/records/save_permanent.json";
-    # Pack cookies (semi colon delimited list of name value pairs)
-    # Pack tsetse_handle, PDS_HANDLE, and tsetse_credentials (if logging in via tsetse[saves a call to PDS]).
-    my $cookies = "tsetse_handle=$tsetse_handle; domain=.library.nyu.edu; path=/, ".
-        "PDS_HANDLE=$pds_handle; domain=.library.nyu.edu; path=/, ";
-    $cookies .= "tsetse_credentials=$tsetse_credentials; domain=.library.nyu.edu; path=/, " if $tsetse_credentials;
-    # PUT the request to the eshelf and get the response.
-    my $user_agent = LWP::UserAgent->new();
-    my $url = URI::URL->new($tsetse_save_permanent_url);
-    my $headers = HTTP::Headers->new('Content-Type' => 'application/json',
-      'Cache-Control' => 'no-cache', 'Cookie' => $cookies);
-    my $request = HTTP::Request->new("PUT", $url, $headers);
-    my $response = $user_agent->request($request);
-    return 0 unless ($response->is_success);
-    return 1;
-  }
-}
-
 1;
