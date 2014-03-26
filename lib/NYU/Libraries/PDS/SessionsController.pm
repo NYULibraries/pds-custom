53d20e76a6e859ede5a84952eadb58373ea7175d
B Alter <barnaby.alter@gmail.com>
2014-03-26 11:54:33 -0400

    Cleaning up messy debugging

--- lib/NYU/Libraries/PDS/SessionsController.pm
+++ lib/NYU/Libraries/PDS/SessionsController.pm
@@ -769,10 +769,7 @@ sub ezborrow {
     } else {
       # Exit with Unauthorized Error
       $self->set('error', "EZBorrow Unauthorized");
-      print "Barcode: ".$self->$current_session->barcode;
-      print "Bor Status: ".$self->$current_session->bor_status;
-      print "Auth Statuses: ".EZBORROW_AUTHORIZED_STATUSES;
-      #return $self->_redirect_to_ezborrow_unauthorized();
+      return $self->_redirect_to_ezborrow_unauthorized();
     }
   } else {
     my $nyu_shibboleth_controller = $self->$nyu_shibboleth_controller();
@@ -794,8 +791,8 @@ sub ezborrow {
           # Redirect to EZ proxy
           return $self->_redirect_to_ezborrow($session, uri_unescape($self->current_url));
         } else {
-          print "Barcode: ".$self->$current_session->barcode;
-          print "Bor Status: ".$self->$current_session->bor_status;
+          print "Barcode: ".$session->barcode;
+          print "Bor Status: ".$session->bor_status;
           print "Auth Statuses: ".EZBORROW_AUTHORIZED_STATUSES;
           # Exit with Unauthorized Error
           $self->set('error', "EZBorrow Unauthorized");
