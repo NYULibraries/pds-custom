0fc31a1b273152158a80e8b20dfafee03af31069
B Alter <barnaby.alter@gmail.com>
2015-03-02 17:08:59 -0500

    Revert changes to make it work again

--- lib/NYU/Libraries/PDS/Session.pm
+++ lib/NYU/Libraries/PDS/Session.pm
@@ -25,10 +25,10 @@ use base qw(Class::Accessor);
 # Assumes the same names as the identities
 # => `institute` is expected by PDS
 # => `expiry_date` is expected by PDS
-my @attributes = qw(id email institution_code institute barcode patron_status
+my @attributes = qw(id email institution_code institute barcode bor_status patron_status
   patron_type name first_name last_name uid nyuidn nyu_shibboleth ns_ldap verification
-    entitlement ill_permission college_code college plif_status dept_code department major_code
-      major ill_library session_id expiry_date remote_address);
+    entitlement objectclass ill_permission college_code college plif_status
+      dept_code department major_code major ill_library session_id expiry_date remote_address);
 __PACKAGE__->mk_ro_accessors(@attributes);
 __PACKAGE__->mk_accessors(qw(calling_system target_url));
 
