c6ec475a6e0e073a746a14c33978652b8d1a4cd0
B Alter <barnaby.alter@gmail.com>
2015-05-06 10:53:06 -0400

    Debugging: force cleanup url

--- lib/NYU/Libraries/PDS/SessionsController.pm
+++ lib/NYU/Libraries/PDS/SessionsController.pm
@@ -494,7 +494,7 @@ sub sso {
       }
     }
   }
-  $target_url = handle_primo_target_url($self->{'conf'}, $target_url, $session);
+  $target_url = handle_primo_target_url($self->{'conf'}, $self->target_url, $self->$current_session);
   $target_url = uri_escape($target_url);
   return $self->$redirect($self->cleanup_url.$target_url);
   # return $self->_redirect_to_target();
